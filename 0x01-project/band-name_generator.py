87809ac96edd5fbb8a4e06c45fcb356ec6dd8ed4
EvansOdhams <evansodhams@gmail.com>
2023-11-02 12:45:19 +0300

    band-name_generator.py

---
+++ 0x01-project/band-name_generator.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+# Prompt the user to add the location where they live
+
+user_location = input("Add the Location where you live: ")
+
+# Prompt the user to add the name of their favorite pet
+user_pet = input("Add the name of your favorite pet: ")
+
+#Print the generated band name on the screen
+
+print("You Band Name is: " + user_location + " " + user_pet)
